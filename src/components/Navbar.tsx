66096d4260e2b397e0b867db8b3d49c414bdec10
Camila Martinez <camilamarb98@gmail.com>
2023-06-03 12:19:04 +0200

    Add Theme Switcher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,32 @@
-import { Link } from "@fluentui/react-components";
+import { Link, Switch } from "@fluentui/react-components";
+import React from "react";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const changeTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  const checkedString = checked ? "dark" : "light";
+
   return (
-    <nav className="flex ">
+    <nav className="px-4 py-2 flex justify-between">
       <div className="m-1 flex items-center gap-1">
         <img
           src="VSCodeIcon.png"
           alt="Visual Studio Code Icon"
-          className="w-6"
+          className="w-8"
         />
-        <h3>Visual Studio Code</h3>
+        <h3 className="text-lg">Visual Studio Code</h3>
       </div>
       <div className="m-1 flex items-center gap-4">
+        <Switch
+          label={checkedString}
+          onChange={changeTheme}
+          value={checkedString}
+        />
         <Link appearance="subtle" href="https://code.visualstudio.com/docs">
           Docs
         </Link>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import {
+  FluentProvider,
+  webLightTheme,
+  webDarkTheme,
+} from "@fluentui/react-components";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
