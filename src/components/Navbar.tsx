594762d7443190ee6e95e75613824495d82c1bda
Camila Martinez <camilamarb98@gmail.com>
2023-06-04 17:56:49 +0200

    Remove Switc, passed to dev branch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,6 @@
-import { Link, Switch } from "@fluentui/react-components";
-import React from "react";
-import { useState } from "react";
+import { Link } from "@fluentui/react-components";
 
 export default function Navbar() {
-  const [checked, setChecked] = useState<boolean>(false);
-
-  const changeTheme = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
-
-  const checkedString: string = checked ? "dark" : "light";
-
   return (
     <nav className="px-4 py-2 flex justify-between">
       <div className="m-1 flex items-center gap-2">
@@ -22,11 +12,6 @@ export default function Navbar() {
         <h3 className="text-lg">Visual Studio Code</h3>
       </div>
       <div className="m-1 flex items-center gap-4">
-        {/* <Switch
-          label={checkedString}
-          onChange={changeTheme}
-          value={checkedString}
-        /> */}
         <Link appearance="subtle" href="https://code.visualstudio.com/docs">
           Docs
         </Link>
